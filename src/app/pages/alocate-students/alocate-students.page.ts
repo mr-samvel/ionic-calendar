09f31ab8d7f366ee240e81b45b7afc4eb09f0925
Samuel Moreira Ransolin <samuel@aliancaconvergencia.com.br>
2020-02-10 12:31:56 -0300

    Na alocaÃ§ao de alunos o vetor de eventos foi convertido para um Map; Foram adicionadas algumas verificaÃ§oes; Faltam os filtros e adicionar de fato os alunos Ã s respectivas aulas.

--- src/app/pages/alocate-students/alocate-students.page.ts
+++ src/app/pages/alocate-students/alocate-students.page.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChildren, AfterViewInit, ViewChild } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { CalendarService } from 'src/app/services/calendar.service';
 import { StudentContainerService } from 'src/app/services/student-container.service';
 import { StudentModel } from 'src/app/models/student.model';
 import { IonicSelectableComponent } from 'ionic-selectable';
 import { CalendarComponent } from 'ionic2-calendar/calendar';
+import { ClassModel } from 'src/app/models/event.model';
 
 @Component({
   selector: 'app-alocate-students',
@@ -15,7 +16,8 @@ export class AlocateStudentsPage implements AfterViewInit {
   @ViewChildren(IonicSelectableComponent) private selectableComponentQuery: any;
   @ViewChild(CalendarComponent, { static: false }) calendarComponent: CalendarComponent;
 
-  public events = [];
+  public events: Map<ClassModel, boolean> = new Map();
+  public eventsArray: Array<ClassModel>;
 
   public collapseStudents: boolean = false;
   public collapseFilters: boolean = true;
@@ -26,11 +28,11 @@ export class AlocateStudentsPage implements AfterViewInit {
   public viewTitle: string;
 
   constructor(private modalController: ModalController, private calendarService: CalendarService,
-    private studentsContainer: StudentContainerService) {
-      this.currentDate = new Date();
-      this.viewTitle = '';
-      this.resetAll();
-     }
+    private studentsContainer: StudentContainerService, private toastController: ToastController) {
+    this.currentDate = new Date();
+    this.viewTitle = '';
+    this.resetAll();
+  }
 
   ngAfterViewInit() {
     this.selectableComponentQuery._results.forEach((selectableComponent: IonicSelectableComponent) => {
@@ -40,13 +42,30 @@ export class AlocateStudentsPage implements AfterViewInit {
       selectableComponent.addButtonText = "Novo";
     });
     this.calendarService.getEventSourceObservable().subscribe(eventSrc => {
-      this.events = eventSrc;
-      this.calendarComponent.loadEvents();
-    })
+      setTimeout(() => {
+        for (let i in eventSrc) {
+          this.events.set(eventSrc[i], false);
+        }
+        this.calendarComponent.loadEvents();
+      }, 0);
+    });
   }
 
   async closeModal() {
     await this.modalController.dismiss();
+  }
+
+  private async presentDangerToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      color: 'danger'
+    });
+    return toast.present();
+  }
+
+  toEventArray() {
+    return Array.from(this.events.keys());
   }
 
   resetAll() {
@@ -59,12 +78,40 @@ export class AlocateStudentsPage implements AfterViewInit {
     this.studentsContainer.addStudent(new StudentModel(stud));
   }
 
-  onEventSelected(ev) {
-    console.log(ev);
+  validateStudentSelectable() {
+    let error: boolean = false;
+    this.events.forEach((value: boolean, ev: ClassModel) => {
+      if (value) {
+        if(this.selectedStudents.length > (ev.studentQt - ev.students.length)) {
+          error = true;
+          this.events.set(ev, false);
+        }
+      }
+    });
+    if (error)
+      this.presentDangerToast('Os eventos que ficariam superlotados foram desmarcados')
+  }
+
+  isSelected(ev: ClassModel) {
+    return this.events.get(ev);
+  }
+
+  onEventSelected(ev:ClassModel) {
+    if(this.selectedStudents.length <= (ev.studentQt - ev.students.length)) {
+      // Adicionar a todas as mesmas classes?
+      this.events.set(ev, !this.events.get(ev));
+    } else {
+      this.presentDangerToast('Erro: mais alunos do que o máximo');
+    }
   }
 
   changeTitle(title) {
     this.viewTitle = title;
   }
 
+  log(...args) {
+    for (let arg of args) {
+      console.log(arg);
+    }
+  }
 }
