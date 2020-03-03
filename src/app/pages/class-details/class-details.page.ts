bb745a547b50f7eaaa6b38fc404cae966fd1eaba
Samuel Moreira Ransolin <samuel@aliancaconvergencia.com.br>
2020-03-03 10:55:02 -0300

    Finalizada opÃ§ao de alocar aluno atraves das visualizacao da aula.

--- src/app/pages/class-details/class-details.page.ts
+++ src/app/pages/class-details/class-details.page.ts
@@ -12,12 +12,14 @@ import { StudentModel } from 'src/app/models/student.model';
 })
 export class ClassDetailsPage implements AfterViewInit {
   public event: ClassModel;
+  public selectedStudents: StudentModel[] = new Array();
   private tempStudents: StudentModel[] = new Array();
 
   constructor(private modalController: ModalController, private alertController: AlertController,
     private studentsContainer: StudentContainerService, private calendarService: CalendarService) { }
 
   ngAfterViewInit() {
+    this.selectedStudents = new Array();
     this.updateTempStudents();
   }
 
@@ -61,8 +63,55 @@ export class ClassDetailsPage implements AfterViewInit {
         {
           text: 'Adicionar',
           handler: (data) => {
-            this.calendarService.addStudentsToClasses([data], [this.event]);
+            this.selectedStudents.push(data);
             this.tempStudents.splice(this.tempStudents.indexOf(data), 1);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  submit() {
+    let temp = this.selectedStudents;
+    this.selectedStudents = new Array();
+    this.addToClasses(temp);
+  }
+
+  private async addToClasses(studs: StudentModel[]) {
+    let eventSource: ClassModel[];
+    let selectedEvents: ClassModel[] = new Array();
+    this.calendarService.getEventSourceObservable().subscribe(eventSrc => {
+      eventSource = eventSrc;
+    });
+    const alert = await this.alertController.create({
+      header: 'Adicionar às aulas?',
+      message: `Adicionar a todas as aulas de ${this.event.modality.name}, 
+        do(a) profissional ${this.event.professional.name}, às ${this.event.startTime.toLocaleTimeString().slice(0, 5)} - 
+        ${this.event.startTime.toLocaleDateString(undefined, { weekday: 'long' })}?`,
+      buttons: [
+        {
+          text: 'Não, apenas essa',
+          handler: () => {
+            this.calendarService.addStudentsToClasses(studs, [this.event]);
+            this.closeModal();
+          }
+        }, {
+          text: 'Sim',
+          handler: () => {
+            eventSource.forEach((ev: ClassModel) => {
+              let vDate = ev.startTime.toLocaleDateString(undefined, { weekday: 'long' }) == this.event.startTime.toLocaleDateString(undefined, { weekday: 'long' });
+              let vStartHour = ev.startTime.getHours() == this.event.startTime.getHours();
+              let vStartMinute = ev.startTime.getMinutes() == this.event.startTime.getMinutes();
+              let vEndHour = ev.endTime.getHours() == this.event.endTime.getHours();
+              let vEndMinute = ev.endTime.getMinutes() == this.event.endTime.getMinutes();
+              let vProf = ev.professional.name == this.event.professional.name;
+              let vMod = ev.modality.name == this.event.modality.name;
+              if (vDate && vStartHour && vStartMinute && vEndHour && vEndHour && vEndMinute && vProf && vMod)
+                selectedEvents.push(ev);
+            });
+            this.calendarService.addStudentsToClasses(studs, selectedEvents);
+            this.closeModal();
           }
         }
       ]
